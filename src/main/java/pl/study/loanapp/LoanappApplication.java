abb8dfe0fff8a32ab723085411247b7073fe0bc4
Michal Mach <m.mach2@DIGITAL.local>
2017-03-04 13:31:33 +0100

    Initial commit for moved project

---
+++ src/main/java/pl/study/loanapp/LoanappApplication.java
@@ -0,0 +1,12 @@
+package pl.study.loanapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanappApplication.class, args);
+	}
+}
